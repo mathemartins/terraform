a06c25e867413ec3cd227d75cc6e7cb0bfd1fb8d
mathemartins <mathegeniuse@gmail.com>
2024-01-07 09:26:26 +0100

    change in access key

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = ""
-  secret_key = ""
+  access_key = "AKIA4MTWMO4EZIVGYM7A"
+  secret_key = "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"
 }
 
 # retrieve list of AZs(Availability Zones) in the region
