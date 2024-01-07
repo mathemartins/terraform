634bdc2f3065e023e74decc06c0e5fd0b7e40872
mathemartins <mathegeniuse@gmail.com>
2024-01-07 18:29:25 +0100

    modified commit for variables

--- main.tf
+++ main.tf
@@ -1,70 +1,183 @@
-provider "google" {
-  credentials = file("./creds.json")
-  project     = var.project_id
-  region      = var.region
+# configure for AWS Provider
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIA4MTWMO4EZIVGYM7A"
+  secret_key = "V/OCMpdzwwMob9Ps6UBQOT49BgPSPiBxEpd3kVe1"
 }
 
-# Task 1: Create a new VPC in your account in the specified region
-resource "google_compute_network" "vpc" {
-  name                    = "my-vpc"
-  auto_create_subnetworks = false
+# retrieve list of AZs(Availability Zones) in the region
+data "aws_availability_zones" "available" {}
+data "aws_region" "current" {}
+
+# define vpc - virtual private cloud
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name        = var.vpc_name
+    Environment = "demo_environment"
+    Terraform   = "true"
+  }
 }
 
-# Task 2: Create public and private subnets in specified Availability Zones
-resource "google_compute_subnetwork" "public_subnets" {
-  count         = length(var.availability_zones)
-  name          = "public-subnet-${count.index + 1}"
-  ip_cidr_range = var.public_subnets_cidr[count.index]
-  region        = var.region
-  network       = google_compute_network.vpc.self_link
+# deploy the private subnets
+resource "aws_subnet" "private_subnets" {
+  vpc_id            = aws_vpc.vpc.id
+  for_each          = var.private_subnets
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value)
+  availability_zone = tolist(data.aws_availability_zones.available.names)[each.value]
+  tags = {
+    Name      = each.key
+    Terraform = "true"
+  }
 }
 
-resource "google_compute_subnetwork" "private_subnets" {
-  count         = length(var.availability_zones)
-  name          = "private-subnet-${count.index + 1}"
-  ip_cidr_range = var.private_subnets_cidr[count.index]
-  region        = var.region
-  network       = google_compute_network.vpc.self_link
+# deploy the public subnets
+resource "aws_subnet" "public_subnets" {
+  vpc_id                  = aws_vpc.vpc.id
+  for_each                = var.public_subnets
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
+  availability_zone       = tolist(data.aws_availability_zones.available.names)[each.value]
+  map_public_ip_on_launch = true
+  tags = {
+    Name      = each.key
+    Terraform = "true"
+  }
 }
 
-# Task 3: Deploy an Internet Gateway and attach it to the VPC
-# Google Cloud doesn't have an Internet Gateway equivalent,
-# as it uses default internet access for public subnets without explicit gateways.
-
-# Task 4: Provision a NAT Gateway (a single instance will do) for outbound connectivity
-resource "google_compute_router" "nat_router" {
-  name    = "my-nat-router"
-  network = google_compute_network.vpc.self_link
-
+# create route-table for public subnets
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+  tags = {
+    Name      = "demo_public_rtb"
+    Terraform = "true"
+  }
 }
 
-# Get the IP address of the NAT gateway
-data "google_compute_router" "nat_gateway_ip" {
-  name    = google_compute_router.nat_router.name
-  region  = var.region
-  project = var.project_id
-  network = google_compute_network.vpc.self_link
+# create route-table for private subnets
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+  tags = {
+    Name      = "demo_private_rtb"
+    Terraform = "true"
+  }
 }
 
-# Task 5: Ensure that route tables are configured to properly route traffic based on the requirements
-resource "google_compute_route" "public_route" {
-  name       = "public-route"
-  network    = google_compute_network.vpc.self_link
-  dest_range = "0.0.0.0/0" # CIDR block for public subnets
-
-  # Define a default route for public subnets via the NAT router's IP address
-  next_hop_ip = data.google_compute_router.nat_gateway_ip.self_link
+# create route-table association for public
+resource "aws_route_table_association" "public" {
+  route_table_id = aws_route_table.public_route_table.id
+  depends_on     = [aws_subnet.public_subnets]
+  for_each       = aws_subnet.public_subnets
+  subnet_id      = each.value.id
 }
 
-resource "google_compute_route" "private_route" {
-  name       = "private-route"
-  network    = google_compute_network.vpc.self_link
-  dest_range = "0.0.0.0/0" # CIDR block for private subnets
-
-  # Define a default route for private subnets via the NAT router's IP address
-  next_hop_ip = data.google_compute_router.nat_gateway_ip.self_link
+# create route-table association for private
+resource "aws_route_table_association" "private" {
+  route_table_id = aws_route_table.private_route_table.id
+  depends_on     = [aws_subnet.private_subnets]
+  for_each       = aws_subnet.private_subnets
+  subnet_id      = each.value.id
 }
 
+# create internet-gateway
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "demo_igw"
+  }
+}
 
+# create elastic IP Address for NAT Gateway
+resource "aws_eip" "nat_gateway_eip" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.internet_gateway]
+  tags = {
+    Name = "demo_igw_eip"
+  }
+}
 
+# create nat-gateway
+resource "aws_nat_gateway" "nat_gateway" {
+  depends_on    = [aws_subnet.public_subnets]
+  allocation_id = aws_eip.nat_gateway_eip.id
+  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+  tags = {
+    Name = "demo-nat-gateway"
+  }
+}
 
+# Terraform data block to lookup latest ubuntu
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  owners = ["099720109477"]
+}
+
+# Terraform resource block to build EC2 instance in the public subnet
+resource "aws_instance" "webserver" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+  tags = {
+    Name = "Ubuntu EC2 Server"
+  }
+}
+
+# provision an s3 resource on amazon
+resource "aws_s3_bucket" "storage_bucket" {
+  bucket = "my-new-terraform-test-${random_id.randomness.id}"
+
+  tags = {
+    Name    = "s3 bucket"
+    Purpose = "intro to resource labs"
+  }
+}
+
+# provision security group to be used later
+resource "aws_security_group" "security_group" {
+  name        = "web-server-inbound"
+  description = "Allow inbound traffic on tcp/443"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "Allow 443 from the internet"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+  }
+
+  tags = {
+    Name    = "web_server_inbound"
+    Purpose = "Intro to resource block lab"
+  }
+}
+
+# provision a random resource locally to use to name certain things randomly
+resource "random_id" "randomness" {
+  byte_length = 1
+}
+
+# adding another subnet
+resource "aws_subnet" "variables_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.250.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name      = "new-variable-subnet"
+    Terraform = true
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,32 +1,50 @@
-variable "project_id" {
-  description = "Your GCP Project ID"
-  default     = "jetbinder"
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
 }
 
-variable "region" {
-  description = "GCP region for deployment"
-  default     = "us-east1"
-}
-
-variable "availability_zones" {
-  type        = list(string)
-  description = "List of availability zones to use"
-  default     = ["us-east1-b", "us-east1-c", "us-east1-d"]
+variable "vpc_name" {
+  type    = string
+  default = "demo-vpc"
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  default     = "10.0.0.0/16"
+  type    = string
+  default = "10.0.0.0/16"
 }
 
-variable "public_subnets_cidr" {
-  type        = list(string)
-  description = "CIDR blocks for public subnets"
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+variable "private_subnets" {
+  default = {
+    "private_subnet_1" = 1
+    "private_subnet_2" = 2
+    "private_subnet_3" = 3
+  }
 }
 
-variable "private_subnets_cidr" {
-  type        = list(string)
-  description = "CIDR blocks for private subnets"
-  default     = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+variable "public_subnets" {
+  default = {
+    "public_subnet_1" = 1
+    "public_subnet_2" = 2
+    "public_subnet_3" = 3
+  }
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for variable subnet"
+  default = "10.0.250.0/24"
+  type = string
+  validation {
+
+  }
+}
+
+variable "subnet_availability_zone" {
+  description = "The subnet variable availability zone"
+  type = string
+}
+
+variable "subnet_auto_ip" {
+  description = "The subnet automatic Ip Address"
+  type = bool
+  default = true
 }
